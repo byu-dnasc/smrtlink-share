7c1db4c56d4d747daa18b274e161883c5bdb4e0b
Ammon Knaupp <adknaupp@gmail.com>
2024-03-15 17:21:57 -0600

    Begin handling of new project, raise OutOfSync for exceptions

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
-from requests import HTTPError
-from smrtlink_client import SmrtLinkClient
 import peewee as pw
+import smrtlink
 
 class Project(pw.Model):
     # camel case for convenience in converting to/from JSON
@@ -17,51 +16,6 @@ class Project(pw.Model):
     def __str__(self):
         return str(self.name)
 
-class DnascSmrtLinkClient(SmrtLinkClient):
-
-    def get_project_dict(self, id):
-        '''
-        Returns a dictionary of project data, or None if not found.
-        Dictionary includes lists of project datasets and members.
-        '''
-        try:
-            return self.get(f"/smrt-link/projects/{id}")
-        except HTTPError as e:
-            assert e.response.status_code == 404, 'Unexpected error when getting project from SMRT Link'
-            return None
-    
-    def get_project_dicts(self):
-        '''
-        Returns a list of dictionaries of project data.
-        Individual project data does not include datasets or members.
-        '''
-        lst = self.get("/smrt-link/projects")
-        projects = []
-        for dct in lst:
-            projects.append(dct)
-        return projects
-        
-    @staticmethod
-    def connect():
-        '''Returns SmrtLinkClient, or exits if cannot connect'''
-        try:
-            return DnascSmrtLinkClient(
-                host="localhost",
-                port=8243,
-                username="admin",
-                password="admin",
-                verify=False # Disable SSL verification (optional, default is True, i.e. SSL verification is enabled)
-            )
-        except Exception as e:
-            print(e)
-            exit(1)
-
-    @staticmethod 
-    def get_instance(): 
-        if not hasattr(DnascSmrtLinkClient, "_instance"): 
-            DnascSmrtLinkClient._instance = DnascSmrtLinkClient.connect() 
-        return DnascSmrtLinkClient._instance
-
 def dict_to_project(dct):
     members = [member['login'] for member in dct['members']]
     dct['members'] = ', '.join(members[1:]) # ignore the first member (project owner)
@@ -70,11 +24,27 @@ def dict_to_project(dct):
 
 def get(id) -> Project:
     '''Returns a Project object, or None if not found.'''
-    sl_client = DnascSmrtLinkClient.get_instance()
+    sl_client = smrtlink.get_client()
     project = sl_client.get_project_dict(id)
     if project:
         return dict_to_project(project)
     return None
+
+class OutOfSyncError(Exception):
+    pass
+
+def get_new():
+    sl_client = smrtlink.get_client()
+    db_ids = Project.select(Project.id)
+    sl_ids = sl_client.get_project_ids()
+
+    offset = len(sl_ids) - len(db_ids)
+    if offset == 0:
+        return None
+    elif offset > 1:
+        raise OutOfSyncError()
+
+    return sl_client.get_project_dict(sl_ids[-1])
 
 def init_db(db_file):
     db = pw.SqliteDatabase(db_file)
@@ -83,14 +53,10 @@ def init_db(db_file):
         db.create_tables([Project], safe=True)
     return db
 
-def get_project_ids():
-    sl_client = DnascSmrtLinkClient.get_instance()
-    return [project['id'] for project in sl_client.get_project_dicts()]
-
 def load_db():
-    sl_client = DnascSmrtLinkClient.get_instance()
+    sl_client = smrtlink.get_client()
     projects = []
-    for id in get_project_ids():
+    for id in sl_client.get_project_ids():
         project_dict = sl_client.get_project_dict(id)
         projects.append(dict_to_project(project_dict))
     Project.bulk_create(projects)
