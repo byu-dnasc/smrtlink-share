92aa9d08c3c17c56c2f07c00474c3aba1bc222e4
Ammon Knaupp <adknaupp@gmail.com>
2024-03-20 17:16:41 -0600

    Add get_dataset method to test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,10 @@ def get_project_dicts():
     with open('tests/projects.json') as f:
         return json.load(f)
 
+def get_dataset_dict():
+    with open('tests/datasets.json') as f:
+        return json.load(f)
+
 @pytest.fixture()
 def sl():
     '''
@@ -14,6 +18,7 @@ def sl():
     class SmrtLinkTestClient:
         '''Mimic DnascSmrtLinkClient for more convenience in testing.'''
         project_dicts = get_project_dicts()
+        dataset_dict = get_dataset_dict()
         def get_project_dict(self, id):
             zero_based_id = id - 1
             if zero_based_id < 0 or zero_based_id >= len(self.project_dicts):
@@ -21,5 +26,9 @@ def sl():
             return self.project_dicts[zero_based_id]
         def get_project_ids(self):
             return [p_d['id'] for p_d in self.project_dicts]
+        def get_dataset(self, uuid):
+            if uuid in self.dataset_dict.keys():
+                return self.dataset_dict[uuid]
+            return None
     smrtlink.DnascSmrtLinkClient._instance = SmrtLinkTestClient()
 
