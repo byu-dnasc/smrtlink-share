0c143c82975abb2efb4a196aa4ca2191c7212c69
Ammon Knaupp <adknaupp@gmail.com>
2024-03-20 13:28:04 -0600

    Test get_dataset method

--- tests/test_smrtlink.py
+++ tests/test_smrtlink.py
@@ -28,3 +28,8 @@ def test_get_project():
 def test_load_db():
     project.load_db()
     assert project.Project.select().count() > 0
+
+def test_get_dataset():
+    uuid = '48a71a3e-c97c-43ea-ba41-8c2b31dd32b2'
+    assert smrtlink.get_client().get_dataset(uuid)['uuid'] == uuid
+    assert not smrtlink.get_client().get_dataset('00000000-0000-0000-0000-000000000001')
