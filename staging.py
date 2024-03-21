29f87d4e3e3702c5792d8ea0f5f2cface99b1779
jwilki23 <wilkinsonjacob22@gmail.com>
2024-03-21 10:11:47 -0600

    Staging test updates

--- staging.py
+++ staging.py
@@ -2,7 +2,7 @@ import project
 import os
 
 def new(project_id):
-    os.mkdir(project_id)
+    os.mkdir(str(project_id))
     name = project.get(project_id).name
-    project_name = os.path.join(project_id, name)
+    project_name = os.path.join(str(project_id), name)
     os.mkdir(project_name)
--- tests/test_staging.py
+++ tests/test_staging.py
@@ -5,5 +5,5 @@ import os
 pytestmark = pytest.mark.usefixtures('sl') # tests in this file use the SMRT Link "test client"
 
 def test_new():
-    staging.new(1)
-    assert os.path.isdir("1/General Project")
+    staging.new(2)
+    assert os.path.isdir("2/Tomato")
