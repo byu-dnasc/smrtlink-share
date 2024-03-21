0ca569926947ec54043e2b47586500f5bb7a3336
Ammon Knaupp <adknaupp@gmail.com>
2024-03-21 11:00:21 -0600

    Add cleanup fixture

--- tests/test_staging.py
+++ tests/test_staging.py
@@ -1,9 +1,14 @@
 import staging
+import shutil
 import pytest
 import os
 
 pytestmark = pytest.mark.usefixtures('sl') # tests in this file use the SMRT Link "test client"
 
+@pytest.fixture(autouse=True)
+def cleanup():
+    shutil.rmtree(staging.root, ignore_errors=True)
+
 def test_new():
     staging.new(2)
-    assert os.path.isdir("2/Tomato")
+    assert os.path.isdir(os.path.join(staging.root, "2/Tomato"))
