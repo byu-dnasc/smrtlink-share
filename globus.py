1c24879024cf304012d93f74bc290f08c10ee2ab
Ammon Knaupp <adknaupp@gmail.com>
2024-04-05 18:03:02 -0600

    Replace config file with config variables

--- globus.py
+++ globus.py
@@ -1,54 +1,35 @@
 import globus_sdk
-import json
 import os
 import peewee as pw
 
-CREDENTIALS_FILE_PATH = f'/home/{os.environ["USER"]}/smrtlink-share/credentials.json' 
-APP_CLIENT_UUID = '762432c3-3ff0-43cc-af07-1ccda52be14b'
-visible_to_identities = [APP_CLIENT_UUID]
+def get_env_var(var):
+    if var not in os.environ:
+        raise KeyError(f'{var} not found in environment variables')
+    return os.environ[var]
+
+CLIENT_ID = get_env_var('GLOBUS_CLIENT_ID')
+CLIENT_SECRET = get_env_var('GLOBUS_CLIENT_SECRET')
+COLLECTION_ID = get_env_var('GLOBUS_COLLECTION_ID')
+ACL_CREATION_SCOPE='urn:globus:auth:scope:transfer.api.globus.org:all'
+visible_to_identities = [CLIENT_ID]
 VISIBLE_TO_URNS = [f'urn:globus:auth:identity:{i}' for i in visible_to_identities]
 
-def _valid_credentials_file_accessible():
-    if os.path.isfile(CREDENTIALS_FILE_PATH):
-        with open(CREDENTIALS_FILE_PATH) as f:
-            j = json.load(f)
-            if 'Globus Client ID' in j and \
-               'Globus Client Secret' in j:
-                return True
-            else:
-                raise KeyError(f'{CREDENTIALS_FILE_PATH} missing property.')
-    else:
-        raise Exception(f'{CREDENTIALS_FILE_PATH} does not exist.')
-
-def _read_id_secret():
-    try:
-        _valid_credentials_file_accessible()
-    except Exception as e:
-        print('Failed to get Globus client credentials: ' + str(e))
-    with open(CREDENTIALS_FILE_PATH) as f:
-        j = json.load(f)
-        return j['Globus Client ID'], j['Globus Client Secret']
-
 def get_authorizer(scope):
-    client_id, client_secret = _read_id_secret()
     return globus_sdk.ClientCredentialsAuthorizer(
         globus_sdk.ConfidentialAppAuthClient(
-            client_id,
-            client_secret
+            CLIENT_ID,
+            CLIENT_SECRET
         ),
         scope
     )
 
-GUEST_COLLECTION_ID = "b550603b-7baa-43fa-b380-939d15549345" # DNASC
-IDENTITY_ID = "19ff6717-c44d-4ab4-983c-1eb2095beba4" # aknaupp@byu.edu
-ACL_CREATION_SCOPE='urn:globus:auth:scope:transfer.api.globus.org:all'
-
 def get_transfer_client():
     try:
         authorizer = get_authorizer(ACL_CREATION_SCOPE)
-        globus_sdk.TransferClient(authorizer=authorizer)
+        return globus_sdk.TransferClient(authorizer=authorizer)
     except globus_sdk.AuthAPIError as e:
         print('Failed to get TransferClient: ' + str(e))
+        return None
 
 transfer_client = get_transfer_client()
 
@@ -61,7 +42,7 @@ def add_acl_rule(user_id, project_path, project_id):
         "permissions": "r",
     }
     try:
-        rule_id = transfer_client.add_endpoint_acl_rule(GUEST_COLLECTION_ID, rule_data)
+        rule_id = transfer_client.add_endpoint_acl_rule(COLLECTION_ID, rule_data)
         AccessRuleId.create(rule_id=rule_id, project_id=project_id)
     except globus_sdk.TransferAPIError as e:
         pass # TODO Log the error
@@ -69,7 +50,7 @@ def add_acl_rule(user_id, project_path, project_id):
         pass # TODO Log the error
 
 def get_acl_rules():
-    return transfer_client.endpoint_acl_list(GUEST_COLLECTION_ID)
+    return transfer_client.endpoint_acl_list(COLLECTION_ID)
 
 def delete_acl_rule(access_rule_id):
     '''
@@ -77,7 +58,7 @@ def delete_acl_rule(access_rule_id):
     Use case 2: access rule is too old (get rule id directly from Globus)
     '''
     try:
-        transfer_client.delete_endpoint_acl_rule(GUEST_COLLECTION_ID, access_rule_id)
+        transfer_client.delete_endpoint_acl_rule(COLLECTION_ID, access_rule_id)
     except globus_sdk.TransferAPIError:
         pass # TODO Log the error
 
