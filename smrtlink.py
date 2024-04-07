7245afdbeb2d26806965fc6920145271ded73f76
Ammon Knaupp <adknaupp@gmail.com>
2024-04-07 00:26:30 -0600

    Simplify client class by converting methods to functions and use env for config

--- smrtlink.py
+++ smrtlink.py
@@ -2,9 +2,7 @@ from requests import HTTPError
 from project import Project
 from smrtlink_client import SmrtLinkClient
 from dataset import DnascDataSet
-
-class OutOfSyncError(Exception):
-    pass
+from app import get_env_var, OutOfSyncError
 
 def _dict_to_project(dct):
     '''
@@ -17,7 +15,7 @@ def _dict_to_project(dct):
 
 class DnascSmrtLinkClient(SmrtLinkClient):
 
-    def _get_project_dict(self, id):
+    def get_project_dict(self, id):
         '''
         Returns a dictionary of project data, or None if not found.
         Dictionary includes lists of project datasets and members.
@@ -28,30 +26,11 @@ class DnascSmrtLinkClient(SmrtLinkClient):
             assert e.response.status_code == 404, 'Unexpected error when getting project from SMRT Link'
             return None
     
-    def _get_project_ids(self):
+    def get_project_ids(self):
         '''Returns the ids of all projects in SMRT Link.'''
         lst = self.get("/smrt-link/projects")
         return [dct['id'] for dct in lst]
-    
-    def get_project(self, id):
-        '''Get a project from SMRT Link by id.'''
-        project_dict = self._get_project_dict(id)
-        if project_dict:
-            return _dict_to_project(project_dict)
-        return None
-
-    def get_new_project(self):
-        db_ids = Project.select(Project.id)
-        sl_ids = self._get_project_ids()
-
-        offset = len(sl_ids) - len(db_ids)
-        if offset == 0:
-            return None
-        elif offset > 1:
-            raise OutOfSyncError()
-
-        return self.get_project(sl_ids[-1])
-    
+   
     def get_dataset(self, uuid):
         '''
         param uuid: The UUID of a dataset of any type
@@ -64,35 +43,48 @@ class DnascSmrtLinkClient(SmrtLinkClient):
         if hasattr(ds, 'numChildren'):
             num_children = ds['numChildren']
         return DnascDataSet(ds['uuid'], ds['path'], ds['name'], num_children)
+
+HOST = get_env_var('SMRTLINK_HOST')
+PORT = get_env_var('SMRTLINK_PORT')
+USER = get_env_var('SMRTLINK_USER')
+PASS = get_env_var('SMRTLINK_PASS')
+
+def _get_smrtlink_client():
+    try:
+        return DnascSmrtLinkClient(
+            host=HOST,
+            port=PORT,
+            username=USER,
+            password=PASS,
+            verify=False # Disable SSL verification (optional, default is True, i.e. SSL verification is enabled)
+        )
+    except Exception as e:
+        return None
+
+CLIENT = _get_smrtlink_client()
     
-    @staticmethod
-    def connect():
-        '''Returns SmrtLinkClient, or exits if cannot connect'''
-        try:
-            return DnascSmrtLinkClient(
-                host="localhost",
-                port=8243,
-                username="admin",
-                password="admin",
-                verify=False # Disable SSL verification (optional, default is True, i.e. SSL verification is enabled)
-            )
-        except Exception as e:
-            print(e)
-            exit(1)
+def get_project(id):
+    '''Get a project from SMRT Link by id.'''
+    project_dict = CLIENT.get_project_dict(id)
+    if project_dict:
+        return _dict_to_project(project_dict)
+    return None
 
-    @staticmethod 
-    def get_instance(): 
-        if not hasattr(DnascSmrtLinkClient, "_instance"): 
-            DnascSmrtLinkClient._instance = DnascSmrtLinkClient.connect() 
-        return DnascSmrtLinkClient._instance
+def get_new_project():
+    db_ids = Project.select(Project.id)
+    sl_ids = CLIENT.get_project_ids()
 
-def get_client():
-    return DnascSmrtLinkClient.get_instance()
+    offset = len(sl_ids) - len(db_ids)
+    if offset == 0:
+        return None
+    elif offset > 1:
+        raise OutOfSyncError()
+
+    return get_project(sl_ids[-1])
 
 def load_db():
-    sl_client = get_client()
     projects = []
-    for id in sl_client.get_project_ids():
-        project_dict = sl_client.get_project_dict(id)
+    for id in CLIENT.get_project_ids():
+        project_dict = CLIENT.get_project_dict(id)
         projects.append(_dict_to_project(project_dict))
     Project.bulk_create(projects)
