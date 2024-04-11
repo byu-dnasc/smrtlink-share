bcaea890bd6be0581630d7ec614e75c8db8adc35
Ammon Knaupp <adknaupp@gmail.com>
2024-04-11 17:12:15 -0600

    Move SmrtLinkTestClient fixture to test_smrtlink_project.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
 import pytest
-import json
 import os
 import peewee as pw
-from app.project import Project, DatasetId
+from app.project import Project, ProjectDataset, Dataset
 
 # Configure environment variables
 import dotenv
@@ -17,39 +16,10 @@ from app.globus import AccessRuleId
 def init_db():
     db = pw.SqliteDatabase(':memory:')
     Project.bind(db)
-    DatasetId.bind(db)
+    ProjectDataset.bind(db)
+    Dataset.bind(db)
     AccessRuleId.bind(db)
-    db.create_tables([Project, DatasetId, AccessRuleId], safe=True)
+    db.create_tables([Project, ProjectDataset, Dataset, AccessRuleId], safe=True)
     yield
     db.close()
-
-def get_project_dicts():
-    with open('tests/projects.json') as f:
-        return json.load(f)
-
-def get_dataset_dict():
-    with open('tests/datasets.json') as f:
-        return json.load(f)
-
-@pytest.fixture()
-def sl():
-    '''
-    Hack to replace the SmrtLinkClient instance with the "test client".
-    '''
-    class SmrtLinkTestClient:
-        '''Mimic DnascSmrtLinkClient for more convenience in testing.'''
-        project_dicts = get_project_dicts()
-        dataset_dict = get_dataset_dict()
-        def get_project_dict(self, id):
-            zero_based_id = id - 1
-            if zero_based_id < 0 or zero_based_id >= len(self.project_dicts):
-                return None
-            return self.project_dicts[zero_based_id]
-        def get_project_ids(self):
-            return [p_d['id'] for p_d in self.project_dicts]
-        def get_dataset(self, uuid):
-            if uuid in self.dataset_dict.keys():
-                return self.dataset_dict[uuid]
-            return None
-    smrtlink.CLIENT = SmrtLinkTestClient()
 
--- tests/test_smrtlink_project.py
+++ tests/test_smrtlink_project.py
@@ -1,74 +1,62 @@
-import json
 import pytest
-import peewee as pw
-import app.project as project
+import json
+from app.project import Project
 import app.smrtlink as smrtlink
 
-pytestmark = pytest.mark.usefixtures('sl') # tests in this file use the SMRT Link "test client"
-
-def get_project_dicts():
+@pytest.fixture()
+def project_dicts_f():
+    '''Fixture to prevent any possible contamination between tests.'''
     with open('tests/projects.json') as f:
         return json.load(f)
 
-@pytest.fixture()
-def projects_f():
-    '''
-    Fixture to provide access to a set of project dictionaries.
-    Using a fixture prevents contamination between tests in the
-    case that project data gets modified.
-    '''
-    return [project.dict_to_project(p_dict) for p_dict in get_project_dicts()]
+class SmrtLinkTestClient:
+    '''Mimic DnascSmrtLinkClient for more convenience in testing.'''
+    def __init__(self, project_dicts):
+        self.project_dicts = project_dicts
+    def get_project_dict(self, id):
+        zero_based_id = id - 1
+        if zero_based_id < 0 or zero_based_id >= len(self.project_dicts):
+            return None
+        return self.project_dicts[zero_based_id]
+    def get_project_ids(self):
+        return [p_d['id'] for p_d in self.project_dicts]
 
-@pytest.fixture()
-def db_projects(request, projects_f):
+@pytest.fixture(autouse=True)
+def install_test_client(project_dicts_f):
+    smrtlink.CLIENT = SmrtLinkTestClient(project_dicts_f)
+
+@pytest.fixture(autouse=True)
+def load_db(request, project_dicts_f):
     '''
-    Fixture to get a subset of projects to populate database. 
-    The subset (default to all projects) is controlled by the
-    db_offset marker.
+    Fixture to load the database with a subset of the projects
+    from SMRT Link. The subset (default to all projects) is 
+    controlled by the db_offset marker.
     '''
-    last_project_index = len(projects_f) # default to no offset
+    last_project_index = len(project_dicts_f) # default to no offset
     marker = request.node.get_closest_marker('db_offset')
     if marker:
         offset_index = -1 # use default offset (-1)
         if len(marker.args) > 0:
             offset_index = -abs(marker.args[0]) # offset is arg[0]
         last_project_index = offset_index
-    return projects_f[0:last_project_index]
-    
-@pytest.fixture(autouse=True)
-def db(db_projects):
-    '''
-    Adapt functionality in project.init_db() for using an in-memory database.
-    Database content is determined by the db_projects fixture.
-    '''
-    db = pw.SqliteDatabase(':memory:')
-    project.Project.bind(db)
-    project.DatasetId.bind(db)
-    db.connect() # open a connection so that the in-memory database stays alive
-    db.create_tables([project.Project], safe=True)
-    for p in db_projects:
-        p.save(force_insert=True) # override query type UPDATE with INSERT
-    yield
-    db.close() # close the final connection to destroy the database
-
+    # load data into the database by instantiating Project objects via the _dict_to_project function
+    [smrtlink._dict_to_project(dct) for dct in project_dicts_f[0:last_project_index]]
+ 
 @pytest.mark.db_offset()
-def test_offset(projects_f):
-    num_db_projects = project.Project.select().count()
-    assert num_db_projects == len(projects_f) - 1
+def test_offset(project_dicts_f):
+    num_db_projects = Project.select().count()
+    assert num_db_projects == len(project_dicts_f) - 1
 
-def test_sl_test_client(projects_f):
-    assert len(smrtlink.get_client().get_project_ids()) == len(projects_f)
-
-def test_get():
-    assert project.get(1).name == 'General Project'
-    assert project.get(999) is None
+def test_get_project():
+    assert smrtlink.get_project(1).name == 'General Project'
+    assert smrtlink.get_project(999) is None
 
 def test_get_new_none():
     '''
     If there are no new projects, i.e. if the database is in sync with
     SMRT Link, then get_new() should return None.
     '''
-    assert project.get_new() is None
+    assert smrtlink.get_new_project() is None
 
 @pytest.mark.db_offset()
 def test_get_new(projects_f):
@@ -84,5 +72,3 @@ def test_get_new_out_of_sync():
     with pytest.raises(project.OutOfSyncError):
         project.get_new()
 
-def test_dataset_table():
-    assert project.DatasetId.table_exists()
