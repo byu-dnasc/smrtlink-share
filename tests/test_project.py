f61abbc9236c1ea0018e4570d6126c93aca34b2f
Ammon Knaupp <adknaupp@gmail.com>
2024-04-11 17:12:57 -0600

    Test Project class

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,8 +1,21 @@
-from tests.conftest import get_project_dicts
 from app.smrtlink import _dict_to_project
-import app.project as project
+from app.project import Project, ProjectDataset, Dataset
+import json
+import pytest
 
-def test_project():
-    proj_dicts = get_project_dicts()
-    proj = _dict_to_project(proj_dicts[1])
-    assert proj
+@pytest.fixture()
+def project_dicts_f():
+    with open('tests/projects.json') as f:
+        return json.load(f)
+
+def test_project(project_dicts_f):
+    proj = _dict_to_project(project_dicts_f[0])
+    assert Project.select().count() == 1
+    assert ProjectDataset.select().count() == 1
+    assert Dataset.select().count() == 1
+    assert len(proj.datasets) == 1
+
+def test_update_project_no_changes(project_dicts_f):
+    proj = _dict_to_project(project_dicts_f[0])
+    proj_again = _dict_to_project(project_dicts_f[0])
+    assert not proj_again.updates
