6ee1d8daa37dc97343762774fd613ba75a59c482
jwilki23 <jwilki23@byu.edu>
2024-04-15 13:09:33 -0600

    Updated update function, created get_user function, updated delete_dir function.

--- app/staging.py
+++ app/staging.py
@@ -1,11 +1,14 @@
 import app.smrtlink as smrtlink
-from os import listdir, mkdir, makedirs, link, path, rename
-from os.path import join, basename, dirname
+from os import listdir, mkdir, makedirs, link, rename, stat, remove, walk
+import pwd
+from os.path import join, basename, dirname, exists
 import shutil
 import app.globus
+from app import get_env_var
 
 root = '/tmp/staging'
 PERMISSION=0o775
+APP_USER = get_env_var("APP_USER")
 
 def make_dir(dir):
     mkdir(dir, PERMISSION)
@@ -21,9 +24,21 @@ def stage_dataset(dir, dataset):
         for filepath in dataset.files:
             link(filepath, join(dir, basename(filepath)))
 
+def get_user(filepath):
+    return pwd.getpwuid(stat(filepath).st_uid).pw_name
+
 def delete_dir(path):
-    if path.exists([path]):
-        shutil.rmtree(path)
+    # if all of the files in a directory belong to us, delete the whole directory, 
+    # if there is one or more file(s) that belong to a specific owner, delete all 
+    # other files that belong to us and leave the other ones. 
+    if not exists(path):
+        return
+    for dirpath, dirnames, filenames in walk(path):
+        for filename in filenames:
+            filepath = join(dirpath, filename)
+            fileowner = get_user(filepath)
+            if fileowner == APP_USER:
+                remove(filepath)
 
 def new(project):
     project_dir = join(root, str(project.id), project.name)
@@ -38,28 +53,23 @@ def new(project):
 
 def update(project):
     project_path = join(root, project.id, project.name)
-    if 'name' in project.updates:
-        # change project directory name
+    if hasattr(project, "rename"):
         new_project_path = join(dirname(project_path), project.name)
         rename(project_path, new_project_path)
-    if 'dataset_ids' in project.updates:
-        pass # compare staged datasets to project datasets
-        updated_dataset_names = [ds.name for ds in project.datasets]
-        project_path = join(project_path, project.name)
-        dataset_folders = [data_set_folder for data_set_folder in listdir(project_path) if path.isdir(path.join(project_path, data_set_folder))]
-        for dataset in updated_dataset_names:
-            if dataset not in dataset_folders:
-                dataset_dir = join(project_path, dataset.name)
-                mkdir(dataset_dir)
-                stage_dataset(dataset_dir, dataset)
-        for dataset in dataset_folders:
+    if hasattr(project, "datasets_to_add"):
+        for dataset_id in project.datasets_to_add:
+            dataset = project.datasets[dataset_id]
+            dataset_dir = join(project_path, dataset.name)
+            make_dir(dataset_dir)
+            stage_dataset(dataset_dir, dataset)
+    if hasattr(project, "datasets_to_remove"):
+        for dataset in project.datasets_to_remove:
+            dataset = project.datasets[dataset_id]
             dataset_path = join(project_path, dataset.name)
-            if dataset not in updated_dataset_names:
-                delete_dir(dataset_path)
-    if 'members' in project.updates:
-        pass # compare ?
-        # adding or removing access rules
-        new_members = magic()
-        remove_members = magic()
-        for member in remove_members:
+            delete_dir(dataset_path)
+    if hasattr(project, "members_to_add"):
+        for member in project.members_to_add:
+            globus.add_access_rule(member, project_path, project.id)
+    if hasattr(project, "members_to_remove"):
+        for member in project.members_to_remove:
             globus.delete_access_rule(member, project.id)
--- tests/test_staging.py
+++ tests/test_staging.py
@@ -5,12 +5,18 @@ import os
 import app.smrtlink as smrtlink
 
 
-pytestmark = pytest.mark.usefixtures('sl') # tests in this file use the SMRT Link "test client"
-
 @pytest.fixture(autouse=True)
 def cleanup():
     shutil.rmtree(staging.root, ignore_errors=True)
+    os.mkdir(staging.root)
 
 def test_new():
     staging.new(2)
     assert os.path.isdir(os.path.join(staging.root, "2/Tomato", "Germany tomato 20 and 21-Cell1 (all samples)"))
+
+def test_delete_dir():
+    path = staging.root + "/test_file_dir"
+    os.mkdir(path)
+    with open(f"{path}/test_file", "w") as f:
+        pass
+    staging.delete_dir(path)
