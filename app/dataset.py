1b4b9781dd960308d23dcc71ef82940273689e53
Ammon Knaupp <adknaupp@gmail.com>
2024-04-19 17:15:54 -0600

    Parent and Child derived classes

--- app/dataset.py
+++ app/dataset.py
@@ -1,7 +1,7 @@
-from pbcore.io.dataset.DataSetIO import DataSet
+from pbcore.io.dataset.DataSetIO import DataSet as DatasetXml
 from pbcore.io.dataset.DataSetMembers import ExternalResource, ExternalResources
 
-def get_file_path(res, file_paths):
+def _get_file_path(res, file_paths):
     '''
     Get the file path of a resource and recurse if it has 
     ExternalResources
@@ -10,18 +10,18 @@ def get_file_path(res, file_paths):
         file_paths.append(res.resourceId)
     if type(res) is ExternalResource: # else is FileIndex
         if len(res.indices) > 0:
-            get_file_paths(res.indices, file_paths)
+            _get_file_paths(res.indices, file_paths)
         if len(res.externalResources) > 0:
-            get_file_paths(res.externalResources, file_paths)
+            _get_file_paths(res.externalResources, file_paths)
     return file_paths
 
-def get_file_paths(resources: ExternalResources, file_paths):
+def _get_file_paths(resources: ExternalResources, file_paths):
     '''Recursively get all file paths from a list of resources'''
     for res in resources:
-        get_file_path(res, file_paths)
+        _get_file_path(res, file_paths)
     return file_paths
 
-def resources_to_file_paths(resources: ExternalResources):
+def _resources_to_file_paths(resources: ExternalResources):
     '''
     Get all file paths from DataSet.externalResources or DataSet.supplementalResources,
     regardless of the number of nodes in the underlying XML tree by using recursion.
@@ -29,44 +29,124 @@ def resources_to_file_paths(resources: ExternalResources):
     - ExternalResources, SupplementalResources is found in PacBioBaseDataModel.xsd
     - DataSet and derivative types are found in PacBioDatasets.xsd
     '''
-    return get_file_paths(resources, [])
+    return _get_file_paths(resources, [])
 
-def get_dataset_files(xml_file) -> list[str]:
+def _get_dataset_files(xml_file) -> list[str]:
     '''Returns all files associated with a dataset XML file.'''
-    ds = DataSet(xml_file)
-    primary_files = resources_to_file_paths(ds.externalResources)
+    ds = DatasetXml(xml_file)
+    primary_files = _resources_to_file_paths(ds.externalResources)
     try: # TODO: how to handle split datasets?
-        assert len(primary_files) == 1, f'Expected one primary file for dataset, found {len(primary_files)}'
+        assert len(primary_files) == 1, f'Expected one resource file for dataset, found {len(primary_files)}'
     except AssertionError:
         pass
-    supplemental_files = resources_to_file_paths(ds.supplementalResources)
+    supplemental_files = _resources_to_file_paths(ds.supplementalResources)
     return primary_files + supplemental_files
 
-def get_sample_xml(sample_res: ExternalResource):
+def _get_xml(xml_res: ExternalResource):
     # one of the resource's externalResources is the sample XML
-    xml_files = [res for res in sample_res.externalResources if res.resourceId.endswith('.xml')]
+    xml_files = [res for res in xml_res.externalResources if res.resourceId.endswith('.xml')]
+    assert xml_files, 'No XML file found in externalResource'
+    return xml_files[0]
+
+def _get_child_dataset_dicts(parent_xml):
+    '''Return a list (generator) of dictionaries of dataset data'''
+    for res in parent_xml.externalResources:
+        subdataset_xml_file = _get_xml(res)
+        child_xml = DatasetXml(subdataset_xml_file)
+        yield {
+            'name': child_xml.name,
+            'uuid': child_xml.uuid,
+            'path': subdataset_xml_file,
+        }
+
+def get_movie_id(xml: DatasetXml) -> str | None:
     try:
-        assert xml_files, 'No XML file found in sample resource'
-        return xml_files[0]
-    except AssertionError:
+        return (xml.metadata['Collections']
+                            ['CollectionMetadata']
+                            .record['attrib']
+                            ['Context'])
+    except Exception as e:
+        # TODO: log error: xml file does not contain movie id
         return None
 
-def get_dataset_files_super(xml_file):
-    ds = DataSet(xml_file)
-    # in a super dataset, the externalResources represent samples 
-    # and have their own XML files
-    for sample_res in ds.externalResources:
-        # ignore any files associated with this resource except the sample XML
-        sample_dataset_xml = get_sample_xml(sample_res)
-        # instead, use the files from the sample dataset XML
-        sample_dataset_files = get_dataset_files(sample_dataset_xml)
+def get_sample_name(xml: DatasetXml) -> str | None:
+    '''
+    Get the sample name from a dataset XML file
+    representing a single sample (i.e. len(BioSamples) == 1)
+    '''
+    try:
+        return (xml.metadata['Collections']
+                            ['CollectionMetadata']
+                            ['WellSample']
+                            ['BioSamples'][0]
+                            .record['attrib']
+                            ['Name']
+                            )
+    except Exception as e:
+        return None
 
-class DnascDataSet:
-    def __init__(self, uuid, xml_file, name, num_children):
-        self.uuid = uuid
-        self.name = name
-        self.is_super = num_children > 0
-        if self.is_super:
-            self.files = get_dataset_files_super(xml_file, num_children)
+def get_well_sample_name(xml: DatasetXml) -> str | None:
+    try:
+        return (xml.metadata['Collections']
+                            ['CollectionMetadata']
+                            ['WellSample']
+                            .record['attrib']
+                            ['Name'])
+    except Exception as e:
+        return None
+
+def get_barcode(xml: DatasetXml) -> str | None:
+    try:
+        return (xml.metadata['Collections']
+                            ['CollectionMetadata']
+                            ['WellSample']
+                            ['BioSamples'][0]
+                            ['DNABarcodes'][0]
+                            .record['attrib']
+                            ['Name'])
+    except Exception as e:
+        return None
+
+class Dataset:
+    def __new__(cls, **kwargs):
+        if 'numChildren' in kwargs and kwargs['numChildren'] > 0:
+            return super(Dataset, cls).__new__(Parent)
         else:
-            self.files = get_dataset_files(xml_file)
+            return super(Dataset, cls).__new__(Child)
+    def __init__(self, **kwargs):
+        self.id = kwargs['uuid']
+        self.name = kwargs['name']
+        self.xml_path = kwargs['path']
+        self.xml = DatasetXml(self.xml_path)
+        self.movie_id = get_movie_id(self.xml)
+        self.files = []
+
+    def dir_path(self):
+        return f'Movie {self.movie_id} - {self.name}'
+
+    def __str__(self):
+        return str(self.name)
+
+class Parent(Dataset):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        child_dataset_dicts = _get_child_dataset_dicts(self.xml)
+        self.child_datasets = [Child(self, **child_dict) for child_dict in child_dataset_dicts]
+        self.files = _resources_to_file_paths(self.xml.supplementalResources)
+    
+class Child(Dataset):
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(**kwargs)
+        self.barcode = get_barcode(self.xml)
+        self.name = get_sample_name(self.xml) # replace DataSet name with BioSample name
+        self.parent = parent
+    
+    def dir_path(self):
+        '''
+        If this instance was not created from a Parent instance, then
+        the parent attribute is None.
+        '''
+        parent_path = f'Movie {self.movie_id}'
+        if self.parent:
+            parent_path = self.parent.dir_path()
+        return f'{parent_path}/{self.name} ({self.barcode})'
--- app/project.py
+++ app/project.py
@@ -1,4 +1,5 @@
 import peewee as pw
+from app.dataset import Dataset, Parent, Child
 
 class Project(pw.Model):
     '''
@@ -26,13 +27,11 @@ class Project(pw.Model):
         if self.name != db_project.name:
             self.old_name = db_project.name
         current_ids = {uuid for uuid in self.datasets.keys()}
-        stale_ids = {ds.id for ds in db_project._datasets}
+        stale_ids = set(db_project._dataset_ids)
         if current_ids - stale_ids:
             self.datasets_to_add = list(current_ids - stale_ids)
         if stale_ids - current_ids:
-            self._ids_of_datasets_to_remove = list(stale_ids - current_ids)
-            self.names_of_datasets_to_remove = [ds.name for ds in db_project._datasets 
-                                                if ds.id in self._ids_of_datasets_to_remove]
+            self.datasets_to_remove = list(stale_ids - current_ids)
         current_members = set(self.members)
         stale_members = set(db_project._members)
         if current_members - stale_members:
@@ -52,7 +51,7 @@ class Project(pw.Model):
         if hasattr(self, '_ids_of_datasets_to_remove'):
             (ProjectDataset.delete()
                             .where(ProjectDataset.project_id == self.id,
-                                   ProjectDataset.dataset_id.in_(self._ids_of_datasets_to_remove))
+                                   ProjectDataset.dataset_id.in_(self.datasets_to_remove))
                             .execute())
         if hasattr(self, 'members_to_add'):
             for member in self.members_to_add:
@@ -103,6 +102,10 @@ class Project(pw.Model):
             assert 'id' in kwargs and 'name' in kwargs, 'Missing project data'
             super().__init__(*args, **kwargs)
             self.datasets = {ds_dct['uuid']: Dataset(**ds_dct) for ds_dct in kwargs['datasets']}
+            # add child datasets to self.datasets
+            for ds in self.datasets.values():
+                if ds is Parent:
+                    self.datasets.update({child.id: child for child in ds.child_datasets})
             self.members = [member['login'] 
                             for member in kwargs['members'] 
                             if member['role'] != 'OWNER']
@@ -111,14 +114,13 @@ class Project(pw.Model):
             if db_project: # get updates using database
                 self._get_updates(db_project)
                 self._update_db()
-            else: # load new project data into database
+            else: # load instance data into database
                 self._insert_db()
         elif 'id' in kwargs and 'name' in kwargs: # internal instance, a.k.a. database instance
             super().__init__(*args, **kwargs) # populate instance with database data in kwargs
             assert hasattr(self, '_dataset_id_refs'), 'ProjectDataset foreign key backref not found'
             assert hasattr(self, '_member_id_refs'), 'ProjectMember foreign key backref not found'
-            self._datasets = [Dataset(uuid=ref.dataset_id,
-                                      name=ref.dataset_name) for ref in self._dataset_id_refs]
+            self._dataset_ids = [str(ref.dataset_id) for ref in self._dataset_id_refs]
             self._members = [str(ref.member_id) for ref in self._member_id_refs]
         else:
             raise ValueError('Missing arguments to Project.__init__')
@@ -126,29 +128,10 @@ class Project(pw.Model):
     def __str__(self):
         return str(self.name)
 
-class Dataset:
-    def __init__(self, **kwargs):
-        '''
-        Two use cases:
-        1. For external Project instances: all four fields are required.
-        2. For internal Project instances: only the first two fields are required.
-
-        Keyword parameters are named after the fields in the SMRT Link API.
-        '''
-        self.id = str(kwargs['uuid'])
-        self.name = kwargs['name']
-        if 'path' in kwargs and 'numChildren' in kwargs:
-            self.path = kwargs['path']
-            self.num_children = kwargs['numChildren']
-
-    def __str__(self):
-        return str(self.name)
-
 class ProjectDataset(pw.Model):
     '''Do not instantiate outside of `Project`.'''
     project_id = pw.ForeignKeyField(Project, backref='_dataset_id_refs')
     dataset_id = pw.UUIDField()
-    dataset_name = pw.CharField()
 
 class ProjectMember(pw.Model):
     '''Do not instantiate outside of `Project`.'''
