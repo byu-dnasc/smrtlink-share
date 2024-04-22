e21c234b2e9eacf4b78c1d844b6152ac49920449
jwilki23 <jwilki23@byu.edu>
2024-04-22 13:24:39 -0600

    Created comments for most of the functions.

--- app/staging.py
+++ app/staging.py
@@ -20,6 +20,9 @@ class DatasetDirectory(pw.Model):
     path = pw.CharField()
 
 def stage_dataset(dir, dataset):
+    """
+    Verifies the type of dataset 
+    """
     if dataset.is_super: # type(files) is dict
         for sample_dir_basename, filepaths in dataset.files.items():
             sample_dir = join(dir, sample_dir_basename)
@@ -34,6 +37,13 @@ def get_user(filepath):
     return pwd.getpwuid(stat(filepath).st_uid).pw_name
 
 def delete_dir(path):
+    """
+    Gets called in a DELETE request. If there is one or more file(s) that belong to a different owner, it will delete 
+    all other files that belong to the app and not touch the others. 
+    After deleting all files belonging to the app, it tries to delete the directory that contained the files if 
+    there are no other files belonging to other uses in that directory. If there are other files, that directory
+    may continue to exist. 
+    """
     # if all of the files in a directory belong to us, delete the whole directory, 
     # if there is one or more file(s) that belong to a specific owner, delete all 
     # other files that belong to us and leave the other ones. 
@@ -46,7 +56,7 @@ def delete_dir(path):
             if fileowner == APP_USER:
                 remove(filepath)
     try:
-        # After deleting all files that belong to the app user, we want to try to delete
+        # After deleting all files that belong to the app user, to try to delete
         # the directory that contained the files if there are no other files belonging
         # to other users in that directory. If there were other files, it is okay
         # for the directory to continue to exist.
@@ -55,6 +65,10 @@ def delete_dir(path):
         pass
 
 def new(project):
+    """
+    Gets called in response to a POST request. Creates directories for the project root, id, and name.
+    Then creates and stages a directory for each dataset in a project's dataset_ids attribute based on the uuids.
+    """
     project_dir = join(root, str(project.id), project.name)
     makedirs(project_dir)
     for uuid in project.dataset_ids:
@@ -66,15 +80,26 @@ def new(project):
         stage_dataset(dataset_dir, dataset)
 
 def get_project_path(project):
+    """
+    Gets the project path by joining the root directory to the project id and project name.
+    """
     project_path = join(root, project.id, project.name)
     return project_path
 
 def rename_project(project):
+    """
+    Gets called in the update function below. It simply replaces the 
+    old project path with the new one. 
+    """
     project_path = get_project_path(project)
     old_project_path = join(dirname(project_path), project.old_name)
     rename(old_project_path, project_path)
 
 def add_datasets(project):
+    """
+    Gets called in the update function below. For each dataset_id in the project.datasets_to_add
+    attribute, a dataset directory is created and staged.
+    """
     project_path = get_project_path(project)
     for dataset_id in project.datasets_to_add:
         dataset = project.datasets[dataset_id]
@@ -83,6 +108,10 @@ def add_datasets(project):
         stage_dataset(dataset_dir, dataset)
 
 def remove_datasets(project):
+    """
+    Gets called in the update function below. For each dataset_id in the project.datasets_to_remove 
+    attribute, the dataset path is queried and deleted.
+    """
     project_path = get_project_path(project)
     for dataset_id in project.datasets_to_remove:
         dataset_path = (DatasetDirectory.select(DatasetDirectory.path)
@@ -90,15 +119,29 @@ def remove_datasets(project):
         delete_dir(dataset_path)
 
 def add_member(project):
+    """
+    Gets called in the update function below. For each member in the project.members_to_add 
+    attribute, the add_access_rule function is called to grant each member an access rule.
+    """
     project_path = get_project_path(project)
     for member in project.members_to_add:
         globus.add_access_rule(member, project_path, project.id)
 
 def remove_member(project):
+    """
+    Gets called in the update function below. For each member in the project.members_to_remove
+    attribute, the delete_access_rule function is called to remove each members' access rule.
+    """
     for member in project.members_to_remove:
         globus.delete_access_rule(member, project.id)
  
 def update(project):
+    """
+    When a PUT request is sent to the SmartLink server, this function will be called. 
+    This function runs through a series of if statements to determine what kinds of updates
+    need to be made to the project. Updates could be changing the name, adding or removing 
+    datasets, or adding or removing members to/from the Globus collection.
+    """
     if hasattr(project, "old_name"):
         rename_project(project)
     if hasattr(project, "datasets_to_add"):
