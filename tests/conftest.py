d8b5fb33662262d23bf7cd0ef9853176338035b5
Ammon Knaupp <adknaupp@gmail.com>
2024-04-23 11:16:18 -0600

    Don't modify env vars

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,10 @@
 import pytest
-import os
 import peewee as pw
 from app.project import Project, ProjectDataset, ProjectMember
 
 # Configure environment variables
 import dotenv
 dotenv.load_dotenv()
-os.environ['GLOBUS_COLLECTION_ID'] = 'test_collection_id'
 
 # import modules that rely on environment variables
 import app.smrtlink as smrtlink
