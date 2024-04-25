f636d9c61142d957bd9977a80f5e55b7c414e978
Ammon Knaupp <adknaupp@gmail.com>
2024-04-25 17:08:47 -0600

    Add app logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 class EnvVarNotFoundError(Exception):
     pass
@@ -10,3 +11,13 @@ def get_env_var(var):
     if var not in os.environ:
         raise EnvVarNotFoundError(f'{var} not found in environment variables')
     return os.environ[var]
+
+logger = logging.getLogger('smrtlink-share')
+logger.setLevel(logging.INFO)
+log_formatter = logging.Formatter(
+    'At %(asctime)s, %(funcName)s (%(filename)s) said "%(message)s"', datefmt='%H:%M:%S'
+)
+logger.addHandler(
+    (logging.FileHandler('smrtlink-share.log')
+            .setFormatter(log_formatter))
+)
