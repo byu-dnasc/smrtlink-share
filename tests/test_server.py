ecaa611747cfe737433488924b05a87a9e29db08
Ammon Knaupp <adknaupp@gmail.com>
2024-04-25 17:13:12 -0600

    Test valid and invalid requests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,48 +1,54 @@
-import http.client
+'''
+These tests are for the App class found in the server.py file.
+The objective here is to check that the server receives requests,
+responds to them, and logs them.
+'''
+from requests import request
 import pytest
 import logging
 import threading
-import os
+import sys
 
 from app.server import App
+import app
 
-log_file = f'{os.path.dirname(__file__)}/app_request.log'
-logging.basicConfig(filename=log_file, level=logging.INFO, force=True)
+app_log = []
+class ListHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.setFormatter(app.log_formatter)
+    def emit(self, record):
+        app_log.append(self.format(record))
+app.logger.handlers = [ListHandler()]
+
+APP_PORT = 9093
 
 @pytest.fixture(autouse=True)
-def app():
-    app = App(('localhost', 9091), logging.getLogger(__name__))
+def setup():
+    try:
+        app = App(('localhost', APP_PORT))
+    except OSError as e:
+        raise Exception(f"Port {APP_PORT} was probably already in use.")
     app_thread = threading.Thread(target=app.run)
     app_thread.start()
     yield
     app.stop()
     app_thread.join()
-    with open(log_file, 'w') as f:
-        pass # clear the log file
+    app_log.clear()
 
-def do_request(method="POST", path="/smrt-link/projects"):
+def valid_request(method, path):
     try:
-        conn = http.client.HTTPConnection("localhost", 9091)
-        try:
-            conn.request(method, path)
-            response = conn.getresponse() # prevent ConnectionAbortedError on the server side
-            if response.status == 200:
-                return True
-            return False
-        except ConnectionRefusedError:
-            print("Connection refused, i.e. no server listening on that port.")
-            return False
-    finally:
-        conn.close()
-
-def test_app_fixture():
-    assert do_request(method="GET")
-    with open(log_file, "r") as f:
-        assert len(f.readlines()) == 1
+        response = request(method, f"http://localhost:{APP_PORT}{path}")
+        return True if response.status_code == 200 else False
+    except Exception as e:
+        raise Exception(f'Request "{method} {path}" failed: {str(e)}', file=sys.stderr)
 
 def test():
-    assert do_request(method="PUT")
-    assert do_request(method="POST")
-    assert do_request(method="DELETE")
-    with open(log_file, "r") as f:
-        assert len(f.readlines()) == 3
+    assert not valid_request("POST", "/smrt-link/projects/1")
+    assert not valid_request("PUT", "/smrt-link/projects")
+    assert not valid_request("DELETE", "/smrt-link/projects")
+    assert valid_request("GET", "/")
+    assert valid_request("POST", "/smrt-link/projects")
+    assert valid_request("PUT", "/smrt-link/projects/1")
+    assert valid_request("DELETE", "/smrt-link/projects/1")
+    assert len(app_log) == 7
