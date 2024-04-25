f9ecd23d616eefdd8df1719d0a2ecff4a3deae37
Ammon Knaupp <adknaupp@gmail.com>
2024-04-25 17:12:31 -0600

    Remove logger configuration, improve log messages, handle invalid requests

--- app/server.py
+++ app/server.py
@@ -3,80 +3,103 @@ from time import sleep
 import app.smrtlink as smrtlink
 import app.staging as staging
 import app.globus as globus
+from app import logger, OutOfSyncError
+import re
+
+PROJECT_PATH = '/smrt-link/projects'
+
+class InvalidRequestError(Exception):
+    pass
 
 def _get_project_id(uri):
-    project_id = uri.split('/')[-1]
-    try:
-        return int(project_id)
-    except ValueError:
+    '''Try to get project ID from URI, return None if not found'''
+    match = re.match(r'/smrt-link/projects/(\d+)', uri)
+    if match:
+        return int(match.group(1))
+    else:
         return None
-
-def _handler_with_logger(logger):
+    
+class RequestHandler(BaseHTTPRequestHandler):
     '''
-    Factory function to create a RequestHandler class ("Dispatcher") with a logger
+    Respond to and log requests, then handle them asynchronously
     '''
-    class Dispatcher(BaseHTTPRequestHandler):
+    def _log_request(self, code):
+        if code == 200:
+            if self.command == 'POST':
+                logger.info(f'Received notification that a new project was created in SMRT Link')
+            elif self.command == 'PUT':
+                logger.info(f'Received notification that project {_get_project_id(self.path)} was modified in SMRT Link')
+            elif self.command == 'DELETE':
+                logger.info(f'Received notification that project {_get_project_id(self.path)} was deleted in SMRT Link')
+            else:
+                logger.info(f'Received request: {self.command} {self.path}')
+        else:
+            logger.info(f'Received invalid request: {self.command} {self.path}')
+    
+    def _get_response_code(self):
+        '''Return the appropriate response code for the request'''
+        assert self.command in ['POST', 'PUT', 'DELETE']
+        if self.command in ['PUT', 'DELETE']:
+            return 200 if _get_project_id(self.path) \
+                        else 404
+        else: # POST
+            return 200 if self.path == '/smrt-link/projects' \
+                        else 404
+            
+    def _respond_log_wait(self):
+        code = self._get_response_code()
+        self.send_response(code) # RESPOND to client/proxy
+        self.end_headers()
+        self._log_request(code) # LOG the request
+        if code == 404:
+            raise InvalidRequestError
+        sleep(1) # WAIT for SMRT Link to process the request
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self._log_request(200)
+        self.wfile.write(b'smrtlink-share app is online')
+
+    def do_PUT(self):
         '''
-        Respond to and log requests, then dispatch a handler
+        Anything may need to be modified, including
+        files, directory names, and access rules
         '''
-        log = logger
-
-        def _log_request(self):
-            content_length = 0
-            if 'Content-Length' in self.headers:
-                content_length = int(self.headers['Content-Length'])
-            self.log.info(f'{self.command} {self.path} {content_length}')
-        
-        def _minimum_viable_response(self):
-            '''
-            Send the minimum viable response to NGINX (proxy)
-            Anything less may work, but an error will be logged
-            '''
-            self.send_response(200)
-            self.end_headers()
-        
-        def _respond_log_wait(self):
-            self._minimum_viable_response() # RESPOND to NGINX
-            self._log_request() # LOG the request
-            sleep(1) # WAIT for SMRT Link to process the request
-        
-        def do_GET(self):
-            self._minimum_viable_response()
-            self._log_request()
-            self.wfile.write(b'smrtlink-share app is online')
-
-        def do_PUT(self):
-            '''
-            Anything may need to be modified, including
-            files, directory names, and access rules
-            '''
+        try:
             self._respond_log_wait()
-            project_id = _get_project_id(self.path)
-            if project_id:
-                pass
+        except InvalidRequestError:
+            return
+        project_id = _get_project_id(self.path)
+        if project_id:
+            pass
 
-        def do_POST(self):
+    def do_POST(self):
+        try:
             self._respond_log_wait()
-            try:
-                project = smrtlink.get_new_project()
-                staging.new(project)
-            except project.OutOfSyncError:
-                pass
+        except InvalidRequestError:
+            return
+        try:
+            project = smrtlink.get_new_project()
+            staging.new(project)
+        except OutOfSyncError:
+            pass
 
-        def do_DELETE(self):
+    def do_DELETE(self):
+        try:
             self._respond_log_wait()
-            project_id = _get_project_id(self.path)
-            if project_id:
-                pass
-            for rule_id in globus.get_project_access_rule_ids(project_id):
-                globus.delete_acl_rule(rule_id)
-
-    return Dispatcher
+        except InvalidRequestError:
+            return
+        project_id = _get_project_id(self.path)
+        if project_id:
+            pass
+        for rule_id in globus.get_project_access_rule_ids(project_id):
+            globus.delete_acl_rule(rule_id)
 
 class App(ThreadingHTTPServer):
 
-    def __init__(self, server_address, logger):
-        super().__init__(server_address, _handler_with_logger(logger))
+    def __init__(self, server_address):
+        super().__init__(server_address, RequestHandler)
 
     def run(self):
         self.serve_forever()
