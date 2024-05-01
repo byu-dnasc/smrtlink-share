17bad3866e3a74fd79273f82e8c9a9153a15ce15
Ammon Knaupp <adknaupp@gmail.com>
2024-05-01 16:42:40 -0600

    Test Project save method

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,3 +1,4 @@
+import peewee
 from app.project import Project, ProjectDataset, ProjectMember
 import json
 import pytest
@@ -33,49 +34,47 @@ PROJECT = {
     ]
 }
 
-def test_project(project_dicts_f):
+def test_project():
     proj = Project(**PROJECT)
+    assert len(proj.datasets) == 3 # the parent dataset in PROJECT and its two child datasets
+    proj.save()
+    assert proj.is_new
     assert Project.select().count() == 1
-    assert ProjectDataset.select().count() == 1
+    assert ProjectDataset.select().count() == 3
     assert ProjectMember.select().count() == 1
-    assert len(proj.datasets) == 1
+    assert pytest.raises(peewee.IntegrityError, proj.save)
 
-def test_update_project_no_changes(project_dicts_f):
-    p_d = PROJECT
-    proj = Project(**p_d)
-    proj_again = Project(**p_d)
+def test_update_project_no_changes():
+    proj = Project(**PROJECT)
+    proj.save()
+    proj_again = Project(**PROJECT)
+    assert not proj_again.is_new
     assert not hasattr(proj_again, 'datasets_to_add')
     assert not hasattr(proj_again, 'datasets_to_remove')
     assert not hasattr(proj_again, 'members_to_add')
     assert not hasattr(proj_again, 'members_to_remove')
 
-def test_update_project_name(project_dicts_f):
-    p_d = PROJECT
-    proj = Project(**p_d)
-    p_d['name'] = 'updated name'
-    proj_updated = Project(**p_d)
+def test_update_project_name():
+    proj = Project(**PROJECT)
+    proj.save()
+    PROJECT['name'] = 'updated name'
+    proj_updated = Project(**PROJECT)
     assert hasattr(proj_updated, 'old_name')
 
-def test_update_project_add_dataset(project_dicts_f):
-    p_d = PROJECT
-    proj = Project(**p_d)
-    new_dataset = copy.deepcopy(p_d['datasets'][0])
+def test_update_project_add_dataset():
+    proj = Project(**PROJECT)
+    proj.save()
+    new_dataset = copy.deepcopy(PROJECT['datasets'][0])
     new_dataset['uuid'] = 'new uuid'
-    p_d['datasets'].append(new_dataset)
-    proj_updated = Project(**p_d)
+    PROJECT['datasets'].append(new_dataset)
+    proj_updated = Project(**PROJECT)
     assert hasattr(proj_updated, 'datasets_to_add')
     assert proj_updated.datasets_to_add == ['new uuid']
 
-def test_update_project_remove_dataset(project_dicts_f):
-    p_d = PROJECT
-    proj = Project(**p_d)
-    ds_removed = p_d['datasets'].pop()
-    proj_updated = Project(**p_d)
+def test_update_project_remove_dataset():
+    proj = Project(**PROJECT)
+    proj.save()
+    PROJECT['datasets'].pop()
+    proj_updated = Project(**PROJECT)
     assert hasattr(proj_updated, 'datasets_to_remove')
-    assert proj_updated.datasets_to_remove == [ds_removed['uuid']]
-
-def test_project_is_new():
-    proj = Project(**PROJECT)
-    assert proj.is_new
-    proj = Project(**PROJECT)
-    assert not proj.is_new
+    assert len(proj_updated.datasets_to_remove) == 3
