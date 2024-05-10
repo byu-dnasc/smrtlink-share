b5d51ff2df9d1b29309583e61c1c8dd9ecb0679f
Ammon Knaupp <adknaupp@gmail.com>
2024-05-10 11:25:43 -0600

    Make some functions private

--- app/staging.py
+++ app/staging.py
@@ -18,7 +18,7 @@ class DatasetDirectory(pw.Model):
     dataset_id = pw.UUIDField()
     path = pw.CharField()
 
-def make_dir(dir):
+def _make_dir(dir):
     """
     Calls the makedirs method.
     PERMISSION gives the owner and the group full permissions. 
@@ -26,17 +26,17 @@ def make_dir(dir):
     """
     makedirs(dir, DIR_PERMISSION, exist_ok=True)
 
-def stage_dataset(dir, dataset):
+def _stage_dataset(dir, dataset):
     """
     For each file in a dataset, the filepath is linked to the dataset directory.
     """
     for filepath in dataset.files:
         link(filepath, join(dir, basename(filepath)))
 
-def get_user(filepath):
+def _get_user(filepath):
     return pwd.getpwuid(stat(filepath).st_uid).pw_name
 
-def delete_dir(path):
+def _delete_dir(path):
     """
     Gets called in a DELETE request. If there is one or more file(s) that belong to a different owner, it will delete 
     all other files that belong to the app and not touch the others. 
@@ -52,7 +52,7 @@ def delete_dir(path):
     for dirpath, dirnames, filenames in walk(path):
         for filename in filenames:
             filepath = join(dirpath, filename)
-            fileowner = get_user(filepath)
+            fileowner = _get_user(filepath)
             if fileowner == APP_USER:
                 remove(filepath)
     try:
@@ -65,45 +65,41 @@ def delete_dir(path):
         pass
 
 def new(project):
-    """
-    Gets called in response to a POST request. Creates directories for the project root, id, and name.
-    Then creates and stages a directory for each dataset in a project's dataset_ids attribute based on the uuids.
-    """
-    project_dir = join(STAGING_ROOT, str(project.id), project.name)
-    make_dir(project_dir)
-    for dataset in project:
+    '''Raises file exceptions'''
+    project_dir = join(STAGING_ROOT, project.dir_name)
+    _make_dir(project_dir)
+    for dataset in project.datasets:
         dataset_dir = join(project_dir, dataset.dir_name())
-        make_dir(dataset_dir)
-        stage_dataset(dataset_dir, dataset)
-        DatasetDirectory.create(dataset_id=dataset.id, path=dataset_dir)
+        _make_dir(dataset_dir)
+        _stage_dataset(dataset_dir, dataset)
 
-def get_project_path(project):
+def _get_project_path(project):
     """
     Gets the project path by joining the root directory to the project id and project name.
     """
     project_path = join(STAGING_ROOT, project.id, project.name)
     return project_path
 
-def rename_project(project):
+def _rename_project(project):
     """
     Gets called in the update function below. It simply replaces the 
     old project path with the new one. 
     """
-    new_project_path = get_project_path(project)
+    new_project_path = _get_project_path(project)
     current_project_path = join(dirname(new_project_path), project.old_name)
     rename(current_project_path, new_project_path)
 
-def add_datasets(project):
-    project_path = get_project_path(project)
+def _add_datasets(project):
+    project_path = _get_project_path(project)
     for dataset_id in project.datasets_to_add:
         dataset = project[dataset_id]
-        project_path = get_project_path(project)
+        project_path = _get_project_path(project)
         dataset_dir = join(project_path, dataset.dir_path())
-        make_dir(dataset_dir)
+        _make_dir(dataset_dir)
         DatasetDirectory.create(dataset_id=dataset_id, path=dataset_dir)
-        stage_dataset(dataset_dir, dataset)
+        _stage_dataset(dataset_dir, dataset)
 
-def remove_datasets(project):
+def _remove_datasets(project):
     """
     Gets called in the update function below. For each dataset_id in the project.datasets_to_remove 
     attribute, the dataset path is queried and deleted.
@@ -111,18 +107,18 @@ def remove_datasets(project):
     for dataset_id in project.datasets_to_remove:
         dataset_path = (DatasetDirectory.select(DatasetDirectory.path)
                                         .where(DatasetDirectory.dataset_id == dataset_id))
-        delete_dir(dataset_path)
+        _delete_dir(dataset_path)
 
-def add_member(project):
+def _add_member(project):
     """
     Gets called in the update function below. For each member in the project.members_to_add 
     attribute, the add_access_rule function is called to grant each member an access rule.
     """
-    project_path = get_project_path(project)
+    project_path = _get_project_path(project)
     for member in project.members_to_add:
         globus.add_access_rule(member, project_path, project.id)
 
-def remove_member(project):
+def _remove_member(project):
     """
     Gets called in the update function below. For each member in the project.members_to_remove
     attribute, the delete_access_rule function is called to remove each members' access rule.
@@ -138,12 +134,12 @@ def update(project):
     datasets, or adding or removing members to/from the Globus collection.
     """
     if hasattr(project, "old_name"):
-        rename_project(project)
+        _rename_project(project)
     if hasattr(project, "datasets_to_add"):
-        add_datasets(project)
+        _add_datasets(project)
     if hasattr(project, "datasets_to_remove"):
-        remove_datasets(project)
+        _remove_datasets(project)
     if hasattr(project, "members_to_add"):
-        add_member(project)
+        _add_member(project)
     if hasattr(project, "members_to_remove"):
-        remove_member(project)
+        _remove_member(project)
