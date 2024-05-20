07cbb6683684d637376b12527b0bb4d108be734f
Ammon Knaupp <adknaupp@gmail.com>
2024-05-20 15:46:56 -0600

    Remove prototypical code

--- app/job.py
+++ app/job.py
@@ -4,32 +4,7 @@ import threading
 from app import logger
 from app.collection import Analysis
 import app.smrtlink as smrtlink
-from app.state import AnalysisModel, LastJobUpdate, ProjectDataset
-
-def _get_job_project_dataset(job_id):
-    rows = (AnalysisModel.select(AnalysisModel.project_id,
-                                 AnalysisModel.dataset_id)
-                         .where(AnalysisModel.analysis_id == job_id)
-                         .execute())
-    if not rows:
-        return None
-    for row in rows:
-        yield row.project_id, row.dataset_id
-
-def get_analyses(datasets, project_id):
-    for ds in datasets:
-        jobs = smrtlink.get_dataset_jobs(ds.id)
-        ids_to_ignore = (AnalysisModel.select(AnalysisModel.analysis_id)
-                                      .where(AnalysisModel.dataset_id == ds.id,
-                                             AnalysisModel.project_id == project_id)
-                                      .execute())
-        for job_d in [j for j in jobs if j['id'] not in ids_to_ignore]:
-            if job_d['state'] == 'SUCCESSFUL':
-                files = smrtlink.get_job_files(job_d['id'])
-                yield Analysis(ds.dir_path, 
-                                job_d['name'], 
-                                job_d['id'], 
-                                files)
+from app.state import LastJobUpdate, ProjectDataset
 
 def _get_new_jobs():
     try:
@@ -84,7 +59,8 @@ def _poll(analysis, staging_function):
         if state in {"SUCCESSFUL", "FAILED", "TERMINATED", "ABORTED"}:
             if state == "SUCCESSFUL":
                 staging_function(analysis)
-                return
+            # else: we are no longer interested in this job
+            return
         t_current = time.time()
         if t_current - t_start > MAX_POLLING_TIME:
             logger.error(f"Failed to handle analysis {analysis.id}: Max polling time ({MAX_POLLING_TIME}s) exceeded.")
