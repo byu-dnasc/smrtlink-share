fe9b6c72e933c3569027f6a4d11c0512d47b5556
Ammon Knaupp <adknaupp@gmail.com>
2024-05-23 18:00:23 -0600

    Revamp

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,58 +1,49 @@
-'''
-These tests are for the App class found in the server.py file.
-The objective here is to check that the server receives requests,
-responds to them, and logs them.
-'''
-from requests import request
+from unittest.mock import patch
 import pytest
-import logging
 import threading
+from requests import post, put, delete
 
-from app.project import Project
-from app.server import App
-import app
+import app.server as server
+from app import APP_PORT
 
-app_log = []
-class ListHandler(logging.Handler):
-    def __init__(self):
-        super().__init__()
-        self.setFormatter(app.log_formatter)
-    def emit(self, record):
-        app_log.append(self.format(record))
-app.logger.handlers = [ListHandler()]
-
-APP_PORT = 9093
-
-def do_request(method, path):
-    try:
-        response = request(method, f"http://localhost:{APP_PORT}{path}")
-        return response.status_code
-    except Exception as e:
-        raise Exception(f'Request "{method} {path}" failed: {e}')
-
-def test_RequestHandler():
-    # get the server running
-    try:
-        app = App(('localhost', APP_PORT))
-    except OSError as e:
-        raise Exception(f"Port {APP_PORT} was probably already in use.")
+@pytest.fixture(scope='session')
+def setup():
+    app = server.App(('localhost', APP_PORT))
     app_thread = threading.Thread(target=app.run)
     app_thread.start()
-
-    # send some invalid requests
-    assert do_request("POST", "/smrt-link/projects/1") == 404
-    assert do_request("PUT", "/smrt-link/projects") == 404
-    assert do_request("DELETE", "/smrt-link/projects") == 404
-
-    # send some valid requests
-    assert do_request("GET", "/") == 200
-    assert do_request("POST", "/smrt-link/projects") == 200
-    assert do_request("PUT", "/smrt-link/projects/1") == 405
-    assert do_request("DELETE", "/smrt-link/projects/1") == 405
-    assert do_request("PUT", "/smrt-link/projects/2") == 200
-    assert do_request("DELETE", "/smrt-link/projects/2") == 200
-
-    # stop the server
+    yield
     app.stop()
     app_thread.join()
-    app_log.clear()
+
+pytestmark = pytest.mark.usefixtures("setup")
+
+@pytest.mark.parametrize("request_, uri, error_code", 
+    (
+        (post, '/smrt-link/projects/1', 404),
+        (put, '/smrt-link/projects', 404),
+        (delete, '/smrt-link/projects', 404),
+        (put, '/smrt-link/projects/1', 405),
+        (delete, '/smrt-link/projects/1', 405)
+    )
+)
+def test_invalid_requests(request_, uri, error_code):
+    with patch('time.sleep'), \
+         patch('app.server.logger') as logger:
+        assert request_(f'http://localhost:{APP_PORT}{uri}').status_code == error_code
+        assert logger.info.called
+
+@pytest.mark.parametrize("request_, uri, handler", 
+    (
+        (post, "/smrt-link/projects", 'app.handling.new_project'),
+        (put, "/smrt-link/projects/2", 'app.handling.update_project'),
+        (delete, "/smrt-link/projects/2", 'app.handling.delete_project'),
+        (post, "/smrt-link/job-manager/jobs/analysis", 'app.handling.update_analyses')
+    )
+)
+def test_valid_requests(request_, uri, handler):
+    with patch(handler) as handling_function, \
+         patch('time.sleep'), \
+         patch('app.server.logger') as logger:
+        assert request_(f'http://localhost:{APP_PORT}{uri}').status_code == 200
+        assert handling_function.called
+        assert logger.info.called
