7c1c5c7935a1371d125fe4152ccba5f1246b9640
Ammon Knaupp <adknaupp@gmail.com>
2024-06-07 17:56:08 -0600

    Refactor, develop

--- app/handle.py
+++ app/handle.py
@@ -12,24 +12,15 @@ def _stage_analyses(completed, pending):
     '''
     for analysis in completed:
         app.filesystem.stage(analysis)
-        app.state.Job.insert(id=analysis.id).execute()
     for completed_analysis in app.job.track(pending):
         app.filesystem.stage(completed_analysis)
-        app.state.Job.insert(id=analysis.id).execute()
-
-def _get_removed_datasets(project_id, dataset_data):
-    current_ids = [ds['uuid'] for ds in dataset_data]
-    return (app.state.Dataset
-                    .select()
-                    .where(app.state.Dataset.project_id == project_id,
-                            app.state.Dataset.id.not_in(current_ids)
-                    .execute()))
 
 def _handle_removed_datasets(project_id, dataset_data):
-    datasets_to_remove = _get_removed_datasets(project_id, dataset_data)
-    for dataset in datasets_to_remove:
+    current_ids = [ds['uuid'] for ds in dataset_data]
+    removed_datasets = app.state.Dataset.get_previous_datasets(project_id, current_ids)
+    for dataset in removed_datasets:
         app.filesystem.remove(dataset)
-        app.globus.remove_permissions(dataset.id)
+        app.globus.remove_permissions(dataset)
 
 def _handle_dataset_analyses(dataset: app.collection.Dataset):
     try:
@@ -39,12 +30,12 @@ def _handle_dataset_analyses(dataset: app.collection.Dataset):
         return
     _stage_analyses(completed, pending)
 
-def _handle_existing_dataset(project_id, dataset, member_ids, new_member_ids):
+def _handle_existing_dataset(project_id, dataset: app.state.Dataset, member_ids, new_member_ids):
     if dataset.project_id == project_id: # visited
         for member_id in new_member_ids:
             app.globus.create_permission(dataset, member_id)
     else: # reassigned
-        app.globus.remove_permissions(dataset.id)
+        app.globus.remove_permissions(dataset)
         for member_id in member_ids:
             app.globus.create_permission(dataset, member_id)
         dataset.project_id = project_id
@@ -55,68 +46,46 @@ def _handle_new_dataset(project_id, dataset_d, member_ids):
         dataset = app.collection.Dataset(**dataset_d)
     except Exception as e:
         app.logger.error(f"Cannot handle SMRT Link dataset {dataset_d['id']}: {e}.")
-        return None
+        return
     if app.filesystem.stage(dataset):
         for member_id in member_ids:
-            permission_id = app.globus.create_permission(dataset, member_id)
-            (app.state.Permission.insert(member_id=member_id,
-                                        dataset_id=dataset.id,
-                                        permission_id=permission_id)
-                                .execute())
-        (app.state.Dataset.insert(project_id=project_id,
-                                        id=dataset.id,
-                                        staging_dir=dataset.dir_path)
-                                .execute())
-        return dataset
-    return None
+            app.globus.create_permission(dataset, member_id)
+        app.state.Dataset.add(dataset, project_id)
+        _handle_dataset_analyses(dataset)
+        if type(dataset) is app.collection.Parent:
+            for child in dataset.child_datasets:
+                if app.filesystem.stage(child):
+                    _handle_dataset_analyses(child)
 
-def _handle_datasets(project_id, dataset_data, member_ids, new_member_ids):
+def _handle_datasets(project_id, dataset_data, member_ids, new_member_ids) -> list[app.state.Dataset]:
     datasets = []
     for dataset_d in dataset_data:
-        dataset = app.state.Dataset.get_or_none(app.state.Dataset.id == dataset_d['uuid'])
+        dataset = app.state.Dataset.get_one(dataset_d['uuid'])
         if dataset is None:
-            dataset = _handle_new_dataset(project_id, dataset_d, member_ids)
-            if dataset:
-                _handle_dataset_analyses(dataset)
-                if type(dataset) is app.collection.Parent:
-                    for child in dataset.child_datasets:
-                        app.filesystem.stage(child)
+            _handle_new_dataset(project_id, dataset_d, member_ids)
         else: # dataset is app.state.Dataset
             _handle_existing_dataset(project_id, dataset, member_ids, new_member_ids)
-            for analysis in app.job.get_new_completed_analyses(dataset):
-                if app.filesystem.stage(analysis): # This staging is redundant, but is intended as a backup in case the app did not receive a notification for a new analysis.
-                    app.state.Job.insert(id=analysis.id).execute()
         datasets.append(dataset)        
     return datasets
 
-def _get_removed_members(project_id, members) -> list[app.state.ProjectMember]:
-    current_ids = [m.member_id for m in members]
-    return (app.state.ProjectMember
-                    .select()
-                    .where(app.state.ProjectMember.project_id == project_id,
-                            app.state.ProjectMember.member_id
-                                                    .not_in(current_ids))
-                    .execute())
-
 def _handle_removed_members(project_id, member_ids, datasets):
-    removed_members = _get_removed_members(project_id, member_ids)
+    removed_members = app.state.ProjectMember.get_previous_members(project_id, member_ids)
     for member in removed_members:
         for dataset in datasets:
-            app.globus.remove_permission(dataset.id, member.member_id)
+            app.globus.remove_permission(dataset, member.member_id)
         member.delete_instance()
 
-def _handle_new_members(project_id, member_ids):
+def _save_new_members(project_id, member_ids):
     new_members = []
     for member_id in member_ids:
-        project_member = app.state.ProjectMember.get_or_none(app.state.ProjectMember.project_id == project_id,
-                                            app.state.ProjectMember.member_id == member_id)
+        project_member = app.state.ProjectMember.get_one(project_id, member_id)
         if project_member is None:
             new_members.append(member_id)
-            app.state.ProjectMember.insert(project_id=project_id, member_id=member_id).execute()
+            app.state.ProjectMember.add(project_id, member_id)
     return new_members
 
 def _handle_project(project_id, project_dataset_data, member_ids):
-    new_member_ids = _handle_new_members(project_id, member_ids)
+    new_member_ids = _save_new_members(project_id, member_ids)
     datasets = _handle_datasets(project_id, project_dataset_data, member_ids, new_member_ids)
     _handle_removed_members(project_id, member_ids, datasets)
     _handle_removed_datasets(project_dataset_data)
@@ -138,12 +107,10 @@ def new_project():
         return
 
 def deleted_project(project_id):
-    datasets = (app.state.Dataset.select()
-                                .where(app.state.Dataset.project_id == project_id)
-                                .execute())
+    datasets = app.state.Dataset.get_multiple(project_id)
     for dataset in datasets:
         app.filesystem.remove(dataset)
-        app.globus.remove_permissions(dataset.id)
+        app.globus.remove_permissions(dataset)
         dataset.delete_instance()
 
 def new_analyses():
