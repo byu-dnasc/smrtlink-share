641d1e824e31c7e0a33db8e903ceef066232b298
Ammon Knaupp <adknaupp@gmail.com>
2024-06-07 17:49:23 -0600

    Add test

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,7 +1,12 @@
-from app.state import LastJobUpdate
+import app.state
+import app
 
 def test_last_job_update():
-    assert LastJobUpdate.time() == '2000-01-01T00:00:00.000Z'
+    assert app.state.LastJobUpdate.time() == '2000-01-01T00:00:00.000Z'
     time = '2016-12-19T16:12:47.014Z'
-    LastJobUpdate.set(time)
-    assert LastJobUpdate.time() == time
+    app.state.LastJobUpdate.set(time)
+    assert app.state.LastJobUpdate.time() == time
+
+def test_permission():
+    app.state.Permission.add('id', 'member_id', 'dataset_id')
+    app.state.Permission.get_by('member_id', 'dataset_id')
