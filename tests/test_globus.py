686542dad1f24e650cbda484cc6685c693969b08
Ammon Knaupp <adknaupp@gmail.com>
2024-06-12 16:09:12 -0600

    Add tests

--- tests/test_globus.py
+++ tests/test_globus.py
@@ -1,56 +1,42 @@
-import app.globus as globus
-import uuid
+import pytest
+import unittest.mock
+
+import app.globus
+import app.state
+import app
 
 IDENTITY_ID = "19ff6717-c44d-4ab4-983c-1eb2095beba4" # aknaupp@byu.edu
 
-class FakeTransferClient:
-    def add_endpoint_acl_rule(*args):
-        return uuid.uuid4()
-    def delete_endpoint_acl_rule(*args):
-        pass
-globus.TRANSFER_CLIENT = FakeTransferClient()
+class Dataset(app.BaseDataset):
+    def __init__(self, uuid, dir_path):
+        self._uuid = uuid
+        self._dir_path = dir_path
+    @property
+    def uuid(self):
+        return self._uuid
+    @property
+    def dir_path(self):
+        return self._dir_path
 
-def test_add_access_rule_live():
-    # Define test data
-    user_id = IDENTITY_ID
-    project_path = "/"
-    expected_rule_data = {
-        "DATA_TYPE": "access",
-        "principal_type": "identity",
-        "principal": user_id,
-        "path": project_path,
-        "permissions": "r",
-    }
+assert app.globus.TRANSFER_CLIENT is not None
 
-    # Call the function
-    rule_id = globus.add_access_rule(user_id, project_path, 'project_id')
+def test_invalid_path():
+    with unittest.mock.patch('app.logger.error') as log_error:
+        dataset = Dataset('uuid', 'invalid')
+        member_id = IDENTITY_ID
+        app.globus.create_permission(dataset, member_id)
+        assert log_error.called
 
-# look up an access rule by id on a live globus collection
-def test_get_acl_rule_live():
-    print(globus.get_access_rules())
+def test_permission_exists():
+    with unittest.mock.patch('app.logger.error') as log_error:
+        dataset = Dataset('uuid', '/test')
+        member_id = IDENTITY_ID
+        app.globus.create_permission(dataset, member_id)
+        assert log_error.called
 
-def test_get_project_access_rule_ids():
-    globus.AccessRuleId.create(
-            rule_id='rule_id', 
-            project_id='project_id', 
-            globus_user_id='user_id'
-        )
-    rule_ids = globus.get_project_access_rule_ids('project_id')
-    assert len(rule_ids) == 1
-
-def test_add_access_rule():
-    user_id = "asdf"
-    project_path = "/"
-    project_id = 1
-    globus.add_access_rule(user_id, project_path, project_id)
-    project_rule_ids = globus.get_project_access_rule_ids(project_id)
-    assert len(project_rule_ids) == 1
-
-def test_delete_access_rule():
-    user_id = "asdf"
-    project_path = "/"
-    project_id = 1
-    globus.add_access_rule(user_id, project_path, project_id)
-    globus.delete_access_rule(user_id, project_id)
-    project_rule_ids = globus.get_project_access_rule_ids(project_id)
-    assert len(project_rule_ids) == 0
+def test_success():
+    with unittest.mock.patch('app.logger.error') as log_error:
+        dataset = Dataset('uuid', '/test')
+        member_id = '65af3497-1ad5-4a79-8c5b-cec928605c1c' # adkna@globusid.org
+        app.globus.create_permission(dataset, member_id)
+        assert not log_error.called
