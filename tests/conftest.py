6599811ad19216050ba5c8ef732e61b1d200c2cb
Ammon Knaupp <adknaupp@gmail.com>
2024-06-21 11:12:46 -0700

    Disable SSL warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
 ADMIN_ID = "19ff6717-c44d-4ab4-983c-1eb2095beba4" # aknaupp@byu.edu
 NON_ADMIN_ID = "65af3497-1ad5-4a79-8c5b-cec928605c1c" # adkna@byu.edu
 DNASC_ID = "45a581b4-78bc-4475-83b0-55849ea0ca11" # dnasc@globusid.org
+
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
