c35848b5e890160511d8925ed70492499a4b23c4
Ammon Knaupp <adknaupp@gmail.com>
2024-06-25 12:51:10 -0600

    Add test data

--- test/data.py
+++ test/data.py
@@ -1,50 +1,65 @@
-TOMATO_PARENT = 'tests/tomatoes/pb_formats/m84100_240301_194028_s1.hifi_reads.consensusreadset.xml'
-TOMATO_20 = 'tests/tomatoes/pb_formats/m84100_240301_194028_s1.hifi_reads.bc1047.consensusreadset.xml'
-TOMATO_21 = 'tests/tomatoes/pb_formats/m84100_240301_194028_s1.hifi_reads.bc1048.consensusreadset.xml'
+PARENT_XML = 'test/tomatoes/pb_formats/m84100_240301_194028_s1.hifi_reads.consensusreadset.xml'
+CHILD_XML_1 = 'test/tomatoes/pb_formats/m84100_240301_194028_s1.hifi_reads.bc1047.consensusreadset.xml'
+CHILD_XML_2 = 'test/tomatoes/pb_formats/m84100_240301_194028_s1.hifi_reads.bc1048.consensusreadset.xml'
+DATASET_XML_1 = CHILD_XML_1 # When a child's parent is not in the project, then it receives no special treatment and thus can serve as a "plain" dataset
+DATASET_XML_2 = CHILD_XML_2 # When a child's parent is not in the project, then it receives no special treatment and thus can serve as a "plain" dataset
 
-PROJECT = {
-    "id": 1,
-    "name": "Tomato Project",
-    "datasets": [
-      {
-        "id": 1,
-        "name": "Germany tomato 20 and 21-Cell1 (all samples)",
-        "numChildren": 2,
-        "uuid": "48a71a3e-c97c-43ea-ba41-8c2b31dd32b2",
-        "path": TOMATO_PARENT
-      }
-    ],
-    "members": [
-      {
-        "login": "admin",
-        "role": "OWNER"
-      },
-      {
-        "login": "superawesomeuser",
-        "role": "CAN_VIEW"
-      }
-    ]
+DATASET_1 = {
+  'id': 1,
+  'name': 'dataset 1 (type: Parent)',
+  'uuid': '1',
+  'numChildren': 2,
+  'path': PARENT_XML
+}
+
+DATASET_2 = {
+  'id': 2,
+  'name': 'dataset 2 (type: Child)',
+  'uuid': '2',
+  'path': CHILD_XML_1
+}
+
+DATASET_3 = {
+  'id': 3,
+  'name': 'dataset 3 (type: Child)',
+  'uuid': '3',
+  'path': CHILD_XML_2
+}
+
+DATASET_4 = {
+  'id': 4,
+  'name': 'dataset 4 (type: Dataset)',
+  'uuid': '4',
+  'path': DATASET_XML_1
+}
+
+DATASET_5 = {
+  'id': 5,
+  'name': 'dataset 5 (type: Dataset)',
+  'uuid': '5',
+  'path': DATASET_XML_1
+}
+
+MEMBER_1 = {
+  "login": "member1",
+  "role": "OWNER"
+}
+
+MEMBER_2 = {
+  "login": "member2",
+  "role": "CAN_VIEW"
 }
 
 PROJECT_1 = {
   'id':1,
-  'name':'project name',
-  'datasets':[
-    {
-      'id': 1,
-      'name': 'dataset name',
-      'uuid': 'a',
-      'path': TOMATO_PARENT
-    }
-  ],
-  "members": [
-    {
-      "login": "admin",
-      "role": "OWNER"
-    },
-    {
-      "login": "superawesomeuser",
-      "role": "CAN_VIEW"
-    }
-  ]
+  'name':'project 1',
+  'datasets':[],
+  "members": []
+}
+
+PROJECT_2 = {
+  'id':2,
+  'name':'project 2',
+  'datasets':[],
+  "members": []
 }
