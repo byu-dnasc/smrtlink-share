5d617ffbcff295c3f79a6e972886c2709a657773
Ammon Knaupp <adknaupp@gmail.com>
2024-06-28 18:06:01 -0600

    Document, use more conventional function names, use typing.Union for return type annotations.

--- app/state.py
+++ app/state.py
@@ -1,4 +1,5 @@
 import datetime
+import typing
 import peewee
 import app
 
@@ -14,24 +15,28 @@ class BaseMeta(peewee.Model.__class__, app.BaseDataset.__class__):
     pass
 
 class Dataset(peewee.Model, app.BaseDataset, metaclass=BaseMeta):
+    '''In contrast to the `app.collection.Dataset` class, this class is used
+    to keep track of the latest known state of a SMRT Link dataset. Specifically,
+    1. the presence of a app.state.Dataset instance whose uuid matches a SMRT Link
+    dataset implies that this dataset has already been handled, and 
+    2. `project_id` stores the id of the project the dataset was last associated
+    with.
+
+    Additionally, instances of this class keep track of where a previously handled
+    dataset's files are stored on the filesystem. This informs us of the file
+    location a project member should be given access to. The file location is
+    also needed when an analysis completes and the file results are to be staged.'''
     uuid = peewee.CharField(primary_key=True, 
                         max_length=36)
     project_id = peewee.IntegerField()
     dir_path = peewee.CharField()
 
     @staticmethod
-    def add(project_id: int, dataset: app.BaseDataset):
-        (Dataset.insert(project_id=project_id,
-                        uuid=dataset.uuid,
-                        dir_path=dataset.dir_path)
-                .execute())
-    
-    @staticmethod
-    def where_dataset_uuid(uuid: str) -> 'Dataset' or None:
+    def get_by_dataset_uuid(uuid: str) -> typing.Union['Dataset', None]:
         return Dataset.get_or_none(Dataset.uuid == uuid)
     
     @staticmethod
-    def where_project_id(project_id: int) -> list['Dataset']:
+    def get_by_project_id(project_id: int) -> list['Dataset']:
         return (Dataset.select()
                        .where(Dataset.project_id == project_id)
                        .execute())
@@ -53,11 +58,6 @@ class ProjectMember(peewee.Model):
     member_id = peewee.CharField()
 
     @staticmethod
-    def add(project_id: int, member_id: str):
-        ProjectMember.insert(project_id=project_id,
-                             member_id=member_id).execute()
-   
-    @staticmethod
     def exists(project_id: int, member_id: str) -> bool:
         return (ProjectMember.select()
                             .where(ProjectMember.project_id == project_id,
@@ -75,18 +75,18 @@ class ProjectMember(peewee.Model):
 class Permission(peewee.Model):
     id = peewee.CharField(primary_key=True)
     member_id = peewee.CharField()
-    dataset_id = peewee.CharField()
+    dataset_uuid = peewee.CharField()
     expiry = peewee.DateTimeField()
 
     @staticmethod
-    def where(member_id: str, dataset_id: str) -> 'Permission' or None:
+    def get_by(member_id: str, dataset_id: str) -> typing.Union['Permission', None]:
         return Permission.get_or_none(Permission.member_id == member_id,
-                                      Permission.dataset_id == dataset_id)
+                                      Permission.dataset_uuid == dataset_id)
     
     @staticmethod
-    def where_dataset_id(dataset_id: str) -> list['Permission']:
+    def get_by_dataset_id(dataset_id: str) -> list['Permission']:
         return (Permission.select()
-                          .where(Permission.dataset_id == dataset_id)
+                          .where(Permission.dataset_uuid == dataset_id)
                           .execute())
     
     @staticmethod
