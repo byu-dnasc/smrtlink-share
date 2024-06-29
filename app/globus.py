21d5842403983d8335b055c172e64b55c39c423f
Ammon Knaupp <adknaupp@gmail.com>
2024-06-28 18:04:49 -0600

    Redo parameters so that only needful data is passed to function.

--- app/globus.py
+++ app/globus.py
@@ -2,7 +2,6 @@ import globus_sdk
 import datetime
 
 import app.state
-import app
 
 ACL_CREATION_SCOPE='urn:globus:auth:scope:transfer.api.globus.org:all'
 
@@ -28,7 +27,7 @@ if app.GLOBUS_CLIENT_ID == '' or app.GLOBUS_CLIENT_SECRET == '':
 else:
     TRANSFER_CLIENT = _get_transfer_client()
 
-def _create_permission(dataset: app.BaseDataset, member_id: str) -> app.state.Permission:
+def _create_permission(dataset_uuid, dataset_dir, member_id: str) -> app.state.Permission:
     '''Raises Globus exception.'''
     expiry = datetime.datetime.now() + \
             datetime.timedelta(days=app.GLOBUS_PERMISSION_DAYS)
@@ -36,14 +35,14 @@ def _create_permission(dataset: app.BaseDataset, member_id: str) -> app.state.Pe
         "DATA_TYPE": "access",
         "principal_type": "identity",
         "principal": member_id,
-        "path": dataset.dir_path,
+        "path": dataset_dir,
         "permissions": "r",
         "expiration_date": expiry.isoformat()
     }
     permission_id = TRANSFER_CLIENT.add_endpoint_acl_rule(app.GLOBUS_COLLECTION_ID, rule_data)
     app.state.Permission.insert(id=permission_id,
                                 member_id=member_id,
-                                dataset_id=dataset.id,
+                                dataset_uuid=dataset_uuid,
                                 expiry=expiry.isoformat()).execute()
 
 def _delete_permission(access_rule_id):
@@ -52,30 +51,30 @@ def _delete_permission(access_rule_id):
     except globus_sdk.TransferAPIError:
         ... # TODO Log the error
 
-def remove_permissions(dataset: app.BaseDataset):
-    permissions = app.state.Permission.where_dataset_id(dataset.uuid)
+def remove_permissions(dataset_uuid, dataset_dir):
+    permissions = app.state.Permission.get_by_dataset_id(dataset_uuid)
     for permission in permissions:
-        remove_permission(dataset, permission.member_id)
+        remove_permission(dataset_uuid, dataset_dir, permission.member_id)
 
-def remove_permission(dataset: app.BaseDataset, member_id: str):
-    permission = app.state.Permission.where(member_id, dataset.uuid)
+def remove_permission(dataset_uuid, dataset_dir, member_id: str):
+    permission = app.state.Permission.get_by(member_id, dataset_uuid)
     if permission is None:
-        app.logger.info(f'Permission for {member_id} to access {dataset.dir_path} not found.')
+        app.logger.info(f'Permission for {member_id} to access {dataset_dir} not found.')
         return
     try:
         _delete_permission(permission.id)
     except globus_sdk.GlobusError as e:
-        app.logger.error(f'Failed to remove permission for {member_id} to access {dataset.dir_path}: {e}')
+        app.logger.error(f'Failed to remove permission for {member_id} to access {dataset_dir}: {e}')
         return
     permission.delete_instance()
 
-def create_permission(dataset: app.BaseDataset, member_id: str):
+def create_permission(dataset_uuid, dataset_dir, member_id: str):
     try:
-        _create_permission(dataset, member_id)
+        _create_permission(dataset_uuid, dataset_dir, member_id)
     except globus_sdk.GlobusAPIError as e:
         message = f'Failed to create Globus permission in collection ' + \
             f'{app.GLOBUS_COLLECTION_ID}: Globus API response message(s): ' \
-            f'{e.message}, dataset.dir_path: {dataset.dir_path}, member_id: {member_id}'
+            f'{e.message}, dataset.dir_path: {dataset_dir}, member_id: {member_id}'
         if e.code == 'Exists':
             app.logger.info(message)
         elif e.code == 'LimitExceeded':
